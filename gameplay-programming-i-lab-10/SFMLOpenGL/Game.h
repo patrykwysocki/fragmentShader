eef9bf14efc4d4e95ece70b4e02f0fc8f6f1481e
Patryk Wysocki <C00213855@itcarlow.ie>
2018-02-15 12:28:34 +0000

    cube drawn no shaders yet, dont know how to do

---
+++ gameplay-programming-i-lab-10/SFMLOpenGL/Game.h
@@ -0,0 +1,40 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <Debug.h>
+
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+#include "MyMatrix3D.h"
+#include "MyVector3D.h"
+
+using namespace std;
+using namespace sf;
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void render();
+	void unload();
+	void transformation(MyMatrix3 &);
+	MyVector3D m_cubePoints[8];
+	MyMatrix3 m_matrix;
+
+	Clock clock;
+	Time elapsed;
+
+	float rotationAngle = 0.0f;
+};
+
+#endif
